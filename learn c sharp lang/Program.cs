d42ad02c65b8ea5853891fc787b70c5d3a8e0e81
whoami <az.khabibullin2018@gmail.com>
2024-05-24 18:05:19 +0500

    прстенькие задачки

--- learn c sharp lang/Program.cs
+++ learn c sharp lang/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace learn_c_sharp_lang
 {
@@ -10,6 +6,33 @@ namespace learn_c_sharp_lang
     {
         static void Main(string[] args)
         {
+            /*
+            1.Напишите программу, вычисляющую среднее арифметическое двух чисел.
+            2.Введите три числа и выведите на экран значение суммы и произведения этих чисел.
+            3.Напишите простой конвертер валют(без возможности динамического выбора валюты пользователем).
+            Валюты заданы хардкодом и не изменяются.Тип валют на выбор программиста.
+            */
+
+            int x = 3;
+            int y = 4;
+            double mean = (x + y) / 2.0;
+            Console.WriteLine(mean);
+
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            int sum, mult;
+            sum = a + b + c;
+            mult = a * b * c;
+            Console.WriteLine(sum + " " + mult);
+
+            int rub = 52;
+            double rubToUsd = rub / 100.0;
+            Console.WriteLine(rubToUsd);
+
+            //костыль
+            Console.ReadLine();
         }
     }
 }
