0c100c6305d6e33d486df5ec2b11541c741e8bb3
whoami <az.khabibullin2018@gmail.com>
2024-05-31 18:53:59 +0500

    задачки из дз 15 16 17

--- learn c sharp lang/Program.cs
+++ learn c sharp lang/Program.cs
@@ -4,21 +4,56 @@ namespace learn_c_sharp_lang
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            /*
-            проверка на четность
-            */
+        //почему мы все методы сделали статическими?
 
-            if (int.Parse(Console.ReadLine()) % 2 == 0)
+        //3. найти сумму цифр числа при помощи рекурсии
+        static int sumDigit(int number)
+        {
+            if (number < 1)
             {
-                Console.WriteLine("Четное");
+                return 0;
             }
             else
             {
-                Console.WriteLine("Нечетное");
+                return number % 10 + sumDigit(number / 10);
             }
-            
+        }
+
+        //2. найти сумму элементов массива при попомщи рекурсии
+        static int sumMasElements(int[]mas, int index = 0)
+        {
+            if (mas.Length <= index)
+            {
+                return 0;
+            }
+            else
+            {
+                return mas[index] + sumMasElements(mas, index + 1);
+            }
+        }
+
+        //1. реализовать вывод массива при помощи рекурсии
+        static void outputMasRecursion(int[]mas, int index = 0)
+        {
+            if (mas.Length > index)
+            {
+                Console.Write(mas[index] + " ");
+                outputMasRecursion(mas, index + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //3
+            //Console.WriteLine(sumDigit(123456789));
+
+            //2 
+            //int [] mas = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Console.WriteLine(sumMasElements(mas));
+
+            //1 
+            int [] mas = { 1 };
+            outputMasRecursion(mas);
 
             //костыль
             Console.ReadLine();
